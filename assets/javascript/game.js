31abef1fa1aa2cbe6bc90642582a2e0194582ac6
casipitj <52521862+juggernautSmash@users.noreply.github.com>
2019-09-12 18:10:13 -0700

    remove carousel, added story board, added feature to kill all other servants

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,58 @@
-// const story = [
-//     `You chosen ${servantName} as the vessel for your`
-// ]
-
 let charAttribute = []
 let userCharacter = {}
+let userBaseAttack = 0
 let servantName
 let enemyCharacter = {}
+let storyCounter = 0
 
-console.log(`userCharacter is : ${Object.keys(userCharacter).length}`)
 
 const renderChar = () => {
-    for(let i=0; i<7; i++){
-        let attackRandomizer = Math.floor(Math.random()*100)+100
-        let defenseRandomizer = Math.floor(Math.random()*100)+100
+        for(let i=0; i<6; i++){
+            let attackRandomizer = Math.floor(Math.random()*100)+100
+            let defenseRandomizer = Math.floor(Math.random()*100)+100
 
-        // console.log(`generating #${i} charAttribute`)
-        getName(i)
-        charAttribute.push({
-            index: i,
-            name: servantName,
-            bhp: 1000,
-            attack: attackRandomizer,
-            defense: defenseRandomizer,
-            rFull: `./assets/images/rf.full${i+1}.png`,
-            lFull: `./assets/images/lf.full${i+1}.png`
-        })
-        console.log(charAttribute[i])
+            // console.log(`generating #${i} charAttribute`)
+            getName(i)
+            charAttribute.push({
+                index: i,
+                name: servantName,
+                bhp: 1000,
+                attack: attackRandomizer,
+                defense: defenseRandomizer,
+                card: `./assets/images/card${i+1}.png`,
+                rFull: `./assets/images/rf.full${i+1}.png`,
+                lFull: `./assets/images/lf.full${i+1}.png`
+            })
+            //console.log(charAttribute[i])
 
-        charElement = document.createElement('div')
-        charElement.className = 'carousel-item'
-        charElement.innerHTML = `
-            <div class="card">
-                <div class="card card-image">
-                    <img class="character" src="./assets/images/card${i+1}.png" 
-                        data-index=${charAttribute[i].index} 
-                        data-bhp="${charAttribute[i].bhp}" 
-                        data-attack=${charAttribute[i].attack} 
-                        data-defense=${charAttribute[i].defense} 
-                        data-rFull=${charAttribute[i].rFull} 
-                        data-lFull=${charAttribute[i].lFull} 
-                        data-name=${charAttribute[i].name}>
+            charElement = document.createElement('div')
+            charElement.className = `col s2`
+            charElement.id = `card${i}`
+            charElement.innerHTML = `
+                <div class="card">
+                    <div class="card card-image">
+                        <img class="character" src=${charAttribute[i].card} 
+                            data-index=${charAttribute[i].index} 
+                            data-bhp=${charAttribute[i].bhp} 
+                            data-attack=${charAttribute[i].attack} 
+                            data-defense=${charAttribute[i].defense} 
+                            data-rFull=${charAttribute[i].rFull} 
+                            data-lFull=${charAttribute[i].lFull} 
+                            data-name=${charAttribute[i].name}>
+                    </div>
+                    <div class="card-title character-attribute">
+                        <p class="card-attribute">HP: ${charAttribute[i].bhp}</p>
+                        <p class="card-attribute">AP: ${charAttribute[i].attack}</p>
+                        <p class="card-attribute">DP: ${charAttribute[i].defense}</p>
+                    </div>
                 </div>
-                <div class="card-title character-attribute">
-                    <p class="card-attribute">HP: ${charAttribute[i].bhp}</p>
-                    <p class="card-attribute">AP: ${charAttribute[i].attack}</p>
-                    <p class="card-attribute">DP: ${charAttribute[i].defense}</p>
-                </div>
-            </div>
-        `
-        document.getElementById('charSelect').append(charElement)        
-    }//end for loop
+            `
+            document.getElementById('charSelect').append(charElement)        
+        }//end for loop
 }// end renderChar
 
 let getName = charIndex => {
-    console.log(`running getName. charIndex is ${charIndex}`)
+    //console.log(`running getName. charIndex is ${charIndex}`)
     charIndex = parseInt(charIndex)
     switch(charIndex){
         case 0:
@@ -85,137 +84,144 @@ let getName = charIndex => {
     //return servantName;
 }// end getName
 
-document.addEventListener('click', event => {
-    // console.log(event)
-    if (event.target.className === 'character' || event.target.className === 'character-attribute') {
-        //console.log(`clicking ${event.target.dataset.name}`)
-        if (Object.keys(userCharacter).length === 0){// Select an avatar
-            userCharacter = {
-                name: event.target.dataset.name,
-                index: event.target.dataset.index,
-                bhp: event.target.dataset.bhp,
-                attack: event.target.dataset.attack,
-                defense: event.target.dataset.defense,
-                rFull: event.target.dataset.rFull,
-                lFull: event.target.dataset.lFull
-            }// end object 
-            console.log(`user servant index: ${userCharacter.index}`)
-            console.log(`userCharacter Object length is ${Object.keys(userCharacter).length}`)
-            console.log(`user object is ${userCharacter}`)
-            renderUserChar(event)
-            removeIntro();
-        }else if(Object.keys(userCharacter).length > 0 && Object.keys(enemyCharacter).length === 0) {
-            enemyCharacter = {
-                name: event.target.dataset.name,
-                index: event.target.dataset.index,
-                bhp: event.target.dataset.bhp,
-                attack: event.target.dataset.attack,
-                defense: event.target.dataset.defense,
-                fullBody: event.target.dataset.fullBody,
-                rFull: event.target.dataset.rFull,
-                lFull: event.target.dataset.lFull
-            }// end object 
-            console.log(`enemy servant index: ${enemyCharacter.index}`)
-            console.log(`enemyCharacter Object length is ${Object.keys(enemyCharacter).length}`)
-            console.log(`enemy object is ${enemyCharacter}`)
-            renderEnemyChar(event)
-        }else{
-            // Play the game
-            // 
-        }// end else statement
-        
-        //console.log(`user selected: ${userCharacter.name}`)
-        // document.getElementById('userSelection').className = "col s4 scale-transition"
-    }// end if
-})
-
 renderUserChar = user => {// Generate user character in the arena
-    let img_Preload = userCharacter.fullBody
     console.log(`running renderUserChar`)
-    console.log(`Index is ${userCharacter.index}`)
-    console.log(`img preload is: ${img_Preload}`)
-    console.log(`click event is ${user.target.dataset.fullBody}`)
-    //getName(event.target.dataset.index)
-    //console.log(`you have chosen ${userCharacter.name}`)
+
+    // Grab attributes from user
+    userCharacter = {
+        name: user.target.dataset.name,
+        index: user.target.dataset.index,
+        bhp: user.target.dataset.bhp,
+        attack: parseInt(user.target.dataset.attack),
+        rFull: user.target.dataset.rFull
+    }// end object 
+
+    // grab the base attack to add on later to the attack
+    userBaseAttack = userCharacter.attack
+
+    // Generate the div to display on the HTML
     userElement = document.createElement('div')
     userElement.innerHTML = `
         <div class="card">
             <div class="card card-image">
-                <img class="user-character" src='${charAttribute[userCharacter.index].lFull}'>
+                <img class="user-character" src=${charAttribute[userCharacter.index].lFull}>
             </div>
             <div class="card-title">
-                <p class="card-attribute-arena">HP: ${userCharacter.bhp}</p>
-                <p class="card-attribute-arena">AP: ${userCharacter.attack}</p>
-                <p class="card-attribute-arena">DP: ${userCharacter.defense}</p>
+                <p class="card-attribute-arena">HP: <span id="userHP">${userCharacter.bhp}</span></p>
+                <p class="card-attribute-arena">AP: <span id="userAttack">${userCharacter.attack}</p>
+                <!-- <p class="card-attribute-arena">DP: ${userCharacter.defense}</p> -->
             </div>
         </div>
     `
-    document.getElementById('userSelection').append(userElement)
-    document.getElementById('userSelection').className = "col s4 scale-transition"
-    //regenerateCharList(userCharacter.index)
-    //delete charAttribute[userCharacter.index]
-    //regenerateCharList()
+    document.getElementById('userChar').append(userElement)
+    document.getElementById('userChar').className = "col s4 scale-transition"
+    document.getElementById('storyProgress').append(`You have chosen ${userCharacter.name} as the vessel for your soul. For your soul to completely bind to the servant you must slay  the other servent vessels and absorb their essence`)
 }
 
-renderEnemyChar = user => {// Generate enemy character in the arena
-    let img_Preload = enemyCharacter.fullBody
-    console.log(`running renderUserChar`)
-    console.log(`Index is ${enemyCharacter.index}`)
-    console.log(`img preload is: ${img_Preload}`)
-    console.log(`click event is ${user.target.dataset.fullBody}`)
-    //getName(event.target.dataset.index)
-    //console.log(`you have chosen ${userCharacter.name}`)
+renderEnemyChar = evilOne => {// Generate enemy character in the arena
+    console.log(`running renderEnemyChar`)
+    
+    //Grab attributtes from evilOne
+    enemyCharacter = {
+        name: evilOne.target.dataset.name,
+        index: evilOne.target.dataset.index,
+        bhp: evilOne.target.dataset.bhp,
+        defense: evilOne.target.dataset.defense,
+        rFull: evilOne.target.dataset.rFull
+    }
+    console.log(`you have chosen evil Servant${enemyCharacter.name}`)
+    console.log(`HP is: ${enemyCharacter.bhp}`)
+    console.log(`DP is: ${enemyCharacter.defense}`)
+    
+    // Generate the div to display on the HTML
     enemyElement = document.createElement('div')
+    enemyElement.id = "enemyStage"
     enemyElement.innerHTML = `
         <div class="card">
             <div class="card card-image">
-                <img class="enemy-character" src='${charAttribute[enemyCharacter.index].rFull}'>
+                <img class="enemy-char" src='${charAttribute[enemyCharacter.index].rFull}'>
             </div>
             <div class="card-title">
-                <p class="card-attribute-arena">HP: ${enemyCharacter.bhp}</p>
-                <p class="card-attribute-arena">AP: ${enemyCharacter.attack}</p>
+                <p class="card-attribute-arena">HP: <span id="enemyHP">${enemyCharacter.bhp}</span></p>
+                <!-- <p class="card-attribute-arena">AP: ${enemyCharacter.attack}</p> -->
                 <p class="card-attribute-arena">DP: ${enemyCharacter.defense}</p>
             </div>
         </div>
     `
-    document.getElementById('enemySelection').append(enemyElement)
-    document.getElementById('enemySelection').className = "col s4 scale-transition"
+    document.getElementById('enemyChar').append(enemyElement)
+    document.getElementById('enemyChar').className = "col s4 scale-transition"
     // delete charAttribute[enemyCharacter.index]
     // regenerateCharList()
-}
+}// end renderEnemyChar
 
+removeCard = div => {
+    console.log(`running removeDiv`)
+    //console.log(`div value is ${div}`)
+    let divBlock = document.getElementById(div)
+    //console.log(`divBlock is ${divBlock}`)
+    let blockChild = document.getElementById(div)
+    divBlock.remove(blockChild)
+}// end removeDiv
 
-// regenerateCharList = (listIndex) => {
-//     console.log(`running regenerateCharList`)
-//     let charElement = ''
-//     document.getElementById('charSelect').r
-//     charAttribute.splice(listIndex, 1)
-//     for(let i=0; i<charAttribute.length; i++){
-//         if(i === userCharacter.index){
-//             console.log(`Index ${userCharacter.index} is the user Index`)
-//         }
-//         charElement = document.createElement('div')
-//         charElement.className = 'carousel-item'
-//         charElement.innerHTML = `
-//             <div class="card">
-//                 <div class="card card-image">
-//                     <img class="character" src="./assets/images/card${i+1}.png" data-index="${charAttribute[i].index}" data-bhp="${charAttribute[i].bhp}" data-attack=${charAttribute[i].attack}" data-defense=${charAttribute[i].defense} data-fullBody="${charAttribute[i].fullBody}" data-name="${charAttribute[i].name}">
-//                 </div>
-//                 <div class="card-title">
-//                     <p class="card-attribute">HP: ${charAttribute[i].bhp}</p>
-//                     <p class="card-attribute">AP: ${charAttribute[i].attack}</p>
-//                     <p class="card-attribute">DP: ${charAttribute[i].defense}</p>
-//                 </div>
-//             </div>
-//         `
-//     }//end for loop
-// }// end regenerateCharList
+document.addEventListener('click', event => {
+    // console.log(event)
+    if (event.target.className === 'character' || event.target.className === 'character-attribute') {
+        if (Object.keys(userCharacter).length === 0){// Select a character
+            storyCounter++
+            renderUserChar(event)
+            removeCard(`card${userCharacter.index}`)
+            removeCard(`intro`)
+        } else if(Object.keys(userCharacter).length > 0 && Object.keys(enemyCharacter).length === 0) {// Select enemy
+            renderEnemyChar(event)
+            removeCard(`card${enemyCharacter.index}`)
+            document.getElementById(`storyProgress`).innerHTML = `You have chosen to absorb ${enemyCharacter.name}'s essence`
+            document.getElementById('fightRow').className = "row scale-transition"
+            document.getElementById("fightButton").className = "waves-effect waves-light btn scale-transition "
+        }
+        else{
+            console.log("Unhandled click event")
+        }
+    }// end if
+})
 
-removeIntro = _ => {
-    let introBlock = document.getElementById('instructions')
-    let storyBlock = document.getElementById('intro')
+document.getElementById("fightButton").addEventListener("click", event =>{
+    userCharacter.bhp -= enemyCharacter.defense
+    enemyCharacter.bhp -= userCharacter.attack
+    console.log(`fightButton is clicked`)
+    //console.log(`enemy HP is ${enemyCharacter.bhp}`)
 
-    introBlock.remove(introBlock)
-}
+    if (userCharacter.bhp > 0 && enemyCharacter.bhp > 0){
+        userCharacter.attack += parseInt(userBaseAttack)
+        document.getElementById('userAttack').innerHTML = userCharacter.attack
+        document.getElementById('userHP').innerHTML = userCharacter.bhp
+        document.getElementById('enemyHP').innerHTML = enemyCharacter.bhp
+        console.log(`enemy HP is ${enemyCharacter.bhp}`)
+    } else if(userCharacter.bhp > 0 && enemyCharacter.bhp <= 0){
+        document.getElementById("storyProgress").innerHTML = `
+        You have slain ${enemyCharacter.name} and absrobed its essence`
+        document.getElementById('enemyHP').innerHTML = '0' // display enemy HP as 0
+        document.getElementById('fightRow').className += ' scale-out' // hide attack button
+        removeCard('enemyStage') // remove the enemy character
+        enemyCharacter = {} // empty the enemt character object
+        userCharacter.bhp += 500 // heal user character by 500 HP
+        document.getElementById('userHP').innerHTML = userCharacter.bhp // display 
+        // console.log(`enemy HP is: ${enemyCharacter.bhp}`)
+        
+    } else if (userCharacter.bhp <= 0 && enemyCharacter.bhp > 0){
+        document.getElementById("storyBoard").innerHTML += "You Lose!"
+        document.getElementById('userHP').innerHTML = '0'
+        document.getElementById('fightRow').className += ' scale-out'
+    } else if (userCharacter.bhp < 0 && enemyCharacter.bhp < 0){
+        document.getElementById("storyBoard").innerHTML += `Draw`
+    } else{
+        console.log('unhandled event')
+    }
+});
+
+const story = [
+    ,
+   ,
+    `you have slain ${enemyCharacter.name} and absrobed its essence`
+]
 
 renderChar()
